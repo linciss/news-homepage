f38b62e0707e521ccf74950bdf8629d644a62925
linards <linardssils1@gmail.com>
2023-09-26 23:13:37 +0300

    Dont for today

--- app/src/Components/Header.js
+++ app/src/Components/Header.js
@@ -4,12 +4,17 @@ function Content() {
   return (
     <>
       <header className="mt-3">
-        <img
-          src={imageWebDesk}
-          alt="isssmage"
-          width="65%"
-          className="header-img"
-        />
+        <span>
+          <img
+            src={imageWebDesk}
+            alt="isssmage"
+            width="60%"
+            className="header-img"
+          />
+          <section className="header-text d-flex">
+            <h1>The Bright Future of Web 3.0?</h1>
+          </section>
+        </span>
       </header>
     </>
   );
